87041d53f0345591db2bb3e05d2129b4fa514a96
Steven Tidd <stidd@aacps.org>
2020-10-14 16:21:52 +0000

    updated class for testing

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,5 @@
 /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,28 +9,59 @@
  *
  * @author steventidd
  */
+
+
+
+
 public class TerrapinCard {
     private double balance;
-    
+
     public TerrapinCard(double balanceAtStart){
         this.balance = balanceAtStart;
     }
-    
+
     //methods
-    public void payEconomical(){
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public String payEconomical(){
         if(this.balance >= 2.50){
             this.balance -= 2.50;
+            return "Your remaining balance is " + this.balance;
+        }else{
+            return "Sorry, you don't have the funds!";
         }
-        
     }
-    
-    public void payGourmet(){
+
+    // public void payEconomical(){
+    //     if(this.balance >= 2.50){
+    //       this.balance -= 2.50;
+    //       System.out.println("Your remaining balance is: " + this.balance);
+    //     }else{
+    //       System.out.println("Sorry, you don't have the funds!");
+    //     }
+    // }
+
+    public String payGourmet(){
         if(this.balance >= 4.00){
-             this.balance -= 4.00;
+            this.balance -= 4.00;
+            return "Your remaining balance is: " + this.balance;
+        }else{
+            return "Sorry, you don't have the funds!";
         }
-       
     }
-    
+
+    // public void payGourmet(){
+    //     if(this.balance >= 4.00){
+    //       this.balance -= 4.00;
+    //       System.out.println("Your remaining balance is: " + this.balance);
+    //     }else{
+    //       System.out.println("Sorry, you don't have the funds!");
+    //     }
+
+    // }
+
     public void loadMoney(double amount){
         if(amount < 0){
             this.balance = this.balance;
@@ -38,13 +70,13 @@ public class TerrapinCard {
         }else{
             this.balance += amount;
         }
-            
+
     }
-    
-    //@Override
+
+    @Override
     public String toString(){
         return "The card has " + this.balance + " dollars";
     }
-    
-    
+
+
 }
