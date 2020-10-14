241c1ea4b10064ab187ca5057df4efaf1463ae70
Steven Tidd <stidd@aacps.org>
2020-10-14 16:24:59 +0000

    updated main method with output

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,8 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-      
+      TerrapinCard billy = new TerrapinCard(50);
+      System.out.println(billy);
 
     }
 }
