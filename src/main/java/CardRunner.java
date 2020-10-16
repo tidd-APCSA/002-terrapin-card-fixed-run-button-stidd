411a9464299df502a1a2dd306acacff479cdc127
Steven Tidd <stidd@aacps.org>
2020-10-16 13:11:21 +0000

    i updated the payGourmet()

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,7 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-      TerrapinCard billy = new TerrapinCard(50);
+      TerrapinCard billy = new TerrapinCard(60);
       System.out.println(billy);
 
     }
